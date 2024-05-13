db5f03a7c422eaae75566cf042e90afa21b3df43
infstka <malder100@tut.by>
2024-05-13 16:50:30 +0300

    add dsn RECORDS! beta version 2.0

--- node.js/profile.js
+++ node.js/profile.js
@@ -4,37 +4,38 @@ const bcrypt = require('bcrypt');
 const db = require('./database.js');
 const verifyToken = require('./user').verifyToken;
 
-// Обновление профиля пользователя
 router.put('/update/:id', verifyToken, async (req, res) => {
   const id = req.params.id;
-  const { username, user_password, user_email } = req.body;
+  const { username = '', user_password = '', user_email = '' } = req.body;
   let hashedPassword = user_password; 
   if (user_password.trim() !== '') { 
     hashedPassword = await bcrypt.hash(user_password, 10);
   }
   
-  // Проверяем уникальность username и user_email
-  const sqlQueryCheck = "CALL check_duplicate_user_except_current(?, ?, ?, @user_count)";
-  db.query(sqlQueryCheck, [id, username, user_email], (err, result) => {
-    db.query('SELECT @user_count', (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Ошибка сервера' });
-      } else if (result[0]['@user_count'] > 0) {
-        // Если нашлось совпадение, возвращаем ошибку
-        res.status(400).json({ error: 'Пользователь с такими данными уже существует!' });
-      } else {
-        // Если не найдено совпадений, выполняем обновление профиля пользователя
-        const sqlQuery = "CALL update_user_profile(?, ?, ?, ?)";
-        db.query(sqlQuery, [id, username, hashedPassword, user_email], (err, result) => {
-          if (err) {
-            res.status(500).json({ error: 'Ошибка обновления профиля' });
-          } else {
-            res.status(200).json({ message: 'Профиль обновлен' });
-          }
-        });
-      }
+  // Проверяем уникальность username и user_email, если они предоставлены
+  if (username.trim() !== '' || user_email.trim() !== '') {
+    const sqlQueryCheck = "CALL check_duplicate_user_except_current(?, ?, ?, @user_count)";
+    db.query(sqlQueryCheck, [id, username, user_email], (err, result) => {
+      db.query('SELECT @user_count', (err, result) => {
+        if (err) {
+          res.status(500).json({ error: 'Ошибка сервера' });
+        } else if (result[0]['@user_count'] > 0) {
+          // Если нашлось совпадение, возвращаем ошибку
+          res.status(400).json({ error: 'Пользователь с такими данными уже существует!' });
+        } else {
+          // Если не найдено совпадений, выполняем обновление профиля пользователя
+          const sqlQuery = "CALL update_user_profile(?, ?, ?, ?)";
+          db.query(sqlQuery, [id, username, hashedPassword, user_email], (err, result) => {
+            if (err) {
+              res.status(500).json({ error: 'Ошибка обновления профиля' });
+            } else {
+              res.status(200).json({ message: 'Профиль обновлен' });
+            }
+          });
+        }
+      });
     });
-  });
+  }
 });
 
 // Получить все бронирования для определенного пользователя
